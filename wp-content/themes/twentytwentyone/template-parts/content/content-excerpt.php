608781fa967683673539848f9beeda4438840ab3
Student <student@mail.edu.vn>
2023-10-23 21:24:06 +0700

    Commit

--- wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
@@ -11,15 +11,36 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class();?>>
+<div class="row top_news_block_desc">
 
+	<?php					
+	$post = get_post();					
+	$date = $post->post_date;					
+	$day = date("j", strtotime($date));					
+	$month = date("F", strtotime($date));					
+	
+	$post_date = get_the_date('d', $post->ID);					
+	$post_month = get_the_date('m', $post->ID);		
+	$post_date = get_the_date('d', $post->ID);					
+	$post_month = get_the_date('m', $post->ID);			
+	
+	
+	echo '<div class="col-md-3 topnewstime">';
+	echo '<span class="topnewsdate">'. $day .'</span>';
+	echo '<br>';
+	echo '<span class="topnewsmonth">'. "Tháng ". $post_month .'</span>';
+	echo '</div>';
+	 ?>
+	 <div class="col-md-9 shortdesc">
 	<?php get_template_part( 'template-parts/header/excerpt-header', get_post_format() ); ?>
 
 	<div class="entry-content">
 		<?php get_template_part( 'template-parts/excerpt/excerpt', get_post_format() ); ?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer default-max-width">
+	</div>
+	<!--<footer class="entry-footer default-max-width">
 		<?php twenty_twenty_one_entry_meta_footer(); ?>
-	</footer><!-- .entry-footer -->
+	</footer> .entry-footer -->
+	</div>
 </article><!-- #post-${ID} -->
--- wp-content/themes/twentytwentyone/template-parts/content/content-single.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-single.php
@@ -13,7 +13,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header alignwide">
+	<header class="entry-headers alignwide">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<?php twenty_twenty_one_post_thumbnail(); ?>
 	</header><!-- .entry-header -->
